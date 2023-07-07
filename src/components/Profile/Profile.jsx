b47f6b8c2f1e5af1bc4d9129113b395f33c85e18
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-07-07 15:14:55 +0200

    hw-prt

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -31,19 +31,13 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
 export default Profile;
 
 Profile.propTypes = {
-  user: PropTypes.objectOf(
-    PropTypes.exact({
-      username: PropTypes.string.isRequired,
-      tag: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-      stats: PropTypes.objectOf(
-        PropTypes.exact({
-          followers: PropTypes.number.isRequired,
-          views: PropTypes.number.isRequired,
-          likes: PropTypes.number.isRequired,
-        }).isRequired
-      ).isRequired,
-    }).isRequired
-  ),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
